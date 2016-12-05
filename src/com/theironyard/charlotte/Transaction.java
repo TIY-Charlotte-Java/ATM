f3a96cb7e5395ad50167c76eedaa3f3d2384784b
GraceConnelly <grace.connelly.a@gmail.com>
2016-12-05 12:24:05 -0500

    Fixed the bug from float point error.

--- src/com/theironyard/charlotte/Transaction.java
+++ src/com/theironyard/charlotte/Transaction.java
@@ -66,20 +66,44 @@ public class Transaction {
         }
     }
 
+//    public static void correctCoinage(double withdrawalAmt) {
+//        double totalBills = 0;
+//        double[][] coinage = {
+//                {100, 20, 10, 5, 1, 0.25, 0.10, 0.05, 0.01},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0}};
+//        String[] coinageName = {"One Hundred dollar bills", "Twenty dollar bill(s)", "Ten dollar bill(s)", "Five dollar bills(s)", "One dollar bills",
+//                "Quarter(s)", "Dime(s)", "Nickel(s)", "Penny(ies)"};
+//        //This code breaks down monies received
+//        System.out.println("You have received $" + withdrawalAmt + " broken down by:");
+//        for (int i = 0; i < coinage[0].length; i++) {
+//            totalBills = totalBills*(double)100;
+//            totalBills = Math.round(totalBills);
+//            totalBills = totalBills/(double)100;
+//
+//            coinage[1][i] = Math.floor((withdrawalAmt - totalBills) / coinage[0][i]);
+//            totalBills = (totalBills + (coinage[1][i] * coinage[0][i]));
+//            if (coinage[1][i] != 0) {
+//                System.out.println(coinage[1][i] + "  " +coinageName[i]);
+//            }
+//        }
     public static void correctCoinage(double withdrawalAmt) {
         double totalBills = 0;
-        double[][] coinage = {
-                {100, 20, 10, 5, 1, 0.25, 0.10, 0.05, 0.01},
+
+        int[][] coinage = {
+                {10000, 2000, 1000, 500, 100, 25, 10, 5, 1},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
         String[] coinageName = {"One Hundred dollar bills", "Twenty dollar bill(s)", "Ten dollar bill(s)", "Five dollar bills(s)", "One dollar bills",
                 "Quarter(s)", "Dime(s)", "Nickel(s)", "Penny(ies)"};
         //This code breaks down monies received
-        System.out.println("You have received $" + withdrawalAmt + " broken down by:");
+        System.out.println("You have received $" + (withdrawalAmt) + " broken down by:");
         for (int i = 0; i < coinage[0].length; i++) {
+
             totalBills = totalBills*(double)100;
             totalBills = Math.round(totalBills);
             totalBills = totalBills/(double)100;
-            coinage[1][i] = Math.floor((withdrawalAmt - totalBills) / coinage[0][i]);
+
+            coinage[1][i] = (int)((withdrawalAmt*100 - totalBills) / coinage[0][i]);
             totalBills = (totalBills + (coinage[1][i] * coinage[0][i]));
             if (coinage[1][i] != 0) {
                 System.out.println(coinage[1][i] + "  " +coinageName[i]);
